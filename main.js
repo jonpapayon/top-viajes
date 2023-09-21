3966b1eb99cc0f526e3088e52eb39abedde7db44
jonpapayon <whyjonpapayon@gmail.com>
2023-09-21 10:14:46 -0600

    fisrt commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#menu')
+const open = document.querySelector('#open')
+const close = document.querySelector('#close')
+
+open.addEventListener('click', () => {
+    menu.classList.add('visible')
+})
+
+close.addEventListener('click', () => {
+    menu.classList.remove('visible')
+})
